6916c9373f18305dbf2b50a50dfac9e8c56af9ab
xunzhang <xunzhangthu@gmail.com>
2015-08-01 11:07:00 -0400

    dt renamed with dfs in path-sum-ii.cc

--- path-sum-ii.cc
+++ path-sum-ii.cc
@@ -10,7 +10,7 @@
 
 class Solution {
  public:
-  void bt(TreeNode* root, int sum,
+  void dfs(TreeNode* root, int sum,
           vector<int> & path, vector<vector<int>> & result) {
     if(root == NULL) return;
     if(root->val == sum && root->left == NULL && root->right == NULL) {
@@ -20,17 +20,17 @@ class Solution {
     }
     vector<int> path_tmp = path;
     path.push_back(root->val);
-    bt(root->left, sum - root->val, path, result);
+    dfs(root->left, sum - root->val, path, result);
     path = path_tmp;
     path.push_back(root->val);
-    bt(root->right, sum - root->val, path, result);
+    dfs(root->right, sum - root->val, path, result);
   }
 
   vector<vector<int>> pathSum(TreeNode* root, int sum) {
     vector<vector<int>> result;
     vector<int> path;
     if(root == NULL) return result;
-    bt(root, sum, path, result);
+    dfs(root, sum, path, result);
     return result;
   }
 };
