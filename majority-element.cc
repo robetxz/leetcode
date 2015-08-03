3a7dad242b8dc869cb06839b56c8d9e2798080f4
xunzhang <xunzhangthu@gmail.com>
2015-08-03 06:39:50 -0400

    majority-element.cc modified, consistent with majority-element-ii.cc

--- majority-element.cc
+++ majority-element.cc
@@ -1,18 +1,17 @@
 class Solution {
  public:
   int majorityElement(vector<int>& nums) {
-    int result = nums[0];
-    int cnt = 1;
-    for(size_t i = 1; i < nums.size(); ++i) {
-      if(nums[i] != result) {
-        cnt -= 1;
+    int result = INT_MAX, cnt = 0;
+    for(size_t i = 0; i < nums.size(); ++i) {
+      if(nums[i] == result) {
+        cnt ++;
+      } else
         if(cnt == 0) {
           result = nums[i];
           cnt = 1;
+        } else {
+          cnt --;
         }
-      } else {
-        cnt += 1;
-      }
     }
     return result;
   }
