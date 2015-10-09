a6e71bab5e0de45106dadababd9454f064012914
xunzhang <xunzhangthu@gmail.com>
2015-10-10 00:43:02 +0800

    course-schedule.cc use bfs cutoff

--- course-schedule.cc
+++ course-schedule.cc
@@ -1,7 +1,7 @@
 class Solution {
 
  public:
-  bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+  bool canFinish_dfs(int numCourses, vector<pair<int, int>>& prerequisites) {
     vector<vector<int>> vp; vp.resize(numCourses);
     for(auto & pr : prerequisites) vp[pr.second].push_back(pr.first);
     while(1) {
@@ -29,5 +29,32 @@ class Solution {
       if(isEmpty) break;
     }
     return true;
+  }
+
+  bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+    if(prerequisites.size() == 0) return true;
+    vector<unordered_set<int>> reverseArrow; reverseArrow.resize(numCourses);
+    unordered_map<int, vector<int>> M;
+    for(auto & pr : prerequisites) {
+      reverseArrow[pr.first].insert(pr.second);
+      M[pr.second].push_back(pr.first);
+    }
+    unordered_set<int> insideSet;
+    int cnt = 0;
+    while(1) {
+      bool exist = false;
+      for(int i = 0; i < numCourses; ++i) {
+        if(insideSet.count(i)) continue;
+        if(reverseArrow[i].size() == 0) {
+          exist = true;
+          insideSet.insert(i);
+          for(auto & j : M[i]) reverseArrow[j].erase(i);
+          break;
+        }
+      }
+      if(!exist) return false;
+      cnt ++;
+      if(cnt == numCourses) return true;
+    }
   }
 };
