3771ec626ec315c90bbf479d4f563b93fcbd5a71
xunzhang <xunzhangthu@gmail.com>
2015-10-14 23:18:58 +0800

    strobogrammatic-number-ii.cc modified

--- strobogrammatic-number-ii.cc
+++ strobogrammatic-number-ii.cc
@@ -21,6 +21,35 @@ class Solution {
     return foo(n);
   }
 
-private:
+ private:
   int N;
+
+// solution which could be extended in strobogrammatic-number-iii
+ public:
+  Solution() {
+    M['0'] = '0'; M['1'] = '1';
+    M['6'] = '9'; M['8'] = '8'; M['9'] = '6';
+  }
+
+  void foo(string tmp, int l, int h) {
+    if(l > h) {
+      if((tmp[0] != '0' || tmp.size() == 1)) rr.push_back(tmp);
+      return;
+    }
+    for(auto & kv : M) {
+      tmp[l] = kv.first;
+      tmp[h] = kv.second;
+      if((l == h && kv.first == kv.second) || l != h) foo(tmp, l + 1, h - 1);
+    }
+  }
+
+  vector<string> findStrobogrammatic(int n) {
+    string tmp(n, ' ');
+    foo(tmp, 0, n - 1);
+    return rr;
+  }
+
+ private:
+  unordered_map<char, char> M;
+  vector<string> rr;
 };
